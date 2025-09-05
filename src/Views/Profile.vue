b786fe667f56b31d68996d4bc24c5a9fc86234a0
JiangWenXuan <3197155053@qq.com>
2025-09-05 17:31:46 +0800

    fiberContent中补充.value; 个人信息界面粗糙版

--- src/Views/Profile.vue
+++ src/Views/Profile.vue
@@ -17,7 +17,41 @@
   </div>
   <!-- ===== Profile Section Start ===== -->
   <section>
-
+    <el-form :model="profile" label-width="160px" style="margin-top:10px ">
+      <el-form-item label="名 称">
+        <span v-show="!xiuGaiZiLiao">{{profile.userName}}</span>
+        <el-input  v-model="profile2.name" v-if="xiuGaiZiLiao" style="width: 90%;"></el-input>
+      </el-form-item>
+      <el-form-item label="真实姓名">
+        <span>{{profile.realName}}</span>
+        <el-input  v-model="profile2.realName" v-if="xiuGaiZiLiao" style="width: 90%;"></el-input>
+      </el-form-item>
+      <el-form-item label="工 号">
+        <span v-show="!xiuGaiZiLiao">{{profile.employeeId}}</span>
+      </el-form-item>
+      <el-form-item label="性 别">
+        <span v-show="!xiuGaiZiLiao">{{profile.gender}}</span>
+        <el-radio v-model="profile2.gender" label="男" v-if="xiuGaiZiLiao"></el-radio>
+        <el-radio v-model="profile2.gender" label="女" v-if="xiuGaiZiLiao"></el-radio>
+      </el-form-item>
+      <el-form-item label="生 日">
+        <span v-show="!xiuGaiZiLiao">{{profile.birth}}</span>
+        <el-input-number  v-model="profile2.birth" :min="0" v-if="xiuGaiZiLiao"></el-input-number>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <span v-show="!xiuGaiZiLiao">{{profile.phone}}</span>
+        <el-input  v-model="profile2.phone" v-if="xiuGaiZiLiao" style="width: 90%;"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <span>{{profile.email}}</span>
+        <el-input  v-model="profile2.email" v-if="xiuGaiZiLiao" style="width: 90%;"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-show="!xiuGaiZiLiao" @click="xiuGaiZiLiao=true;profile2=profile">admend</el-button>
+        <el-button type="primary" @click="xiuGaiZiLiao=false" v-show="xiuGaiZiLiao">cancle</el-button>
+        <el-button type="primary" @click="onSubmit" v-show="xiuGaiZiLiao">commit</el-button>
+      </el-form-item>
+    </el-form>
   </section>
   <!-- ===== Profile Section End ===== -->
 
