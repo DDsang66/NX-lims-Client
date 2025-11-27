b132b357cb4b2d234231d3481bcf99d52b682300
JiangWenXuan <3197155053@qq.com>
2025-11-27 11:42:15 +0800

    Next套餐修改

--- src/Views/Review/Next.vue
+++ src/Views/Review/Next.vue
@@ -14,8 +14,8 @@
   const menuName = ref();
   const orderNumber = ref();
   const menuOptions = ref([
-    { value: 'Woven(Next)', label: 'Woven' },
-    { value: 'Knit(Next)', label: 'Knit' }
+    { value: 'Regular(Next)', label: 'Woven' },
+    { value: 'Seldom(Next)', label: 'Knit' }
   ])
   const additionalRequire = ref();
   const sampleDescription = ref();
