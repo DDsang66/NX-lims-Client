171dcc8b5772f82c4b5176f1611e5c8b6b09abce
JiangWenXuan <3197155053@qq.com>
2025-12-12 15:58:05 +0800

    审单界面去掉required info

--- src/Views/Review/Next.vue
+++ src/Views/Review/Next.vue
@@ -131,10 +131,7 @@
              @api-response="onBuyerData"
              @api-error="handleError"
              @field-change="handleFieldChange"/>
-          <div style="border: 1px solid #cae2e8;border-bottom: none;padding: 0 10px">
-            <label style="font-size: 30px;font-weight: bold">Required Info</label>
-          </div>
-          <div style="border: 1px solid #cae2e8;border-top: none">
+          <div style="border: 1px solid #cae2e8;">
             <RequireLabel
              :buyer="currentBuyer"
              :orderNumber="orderNumber"
